9d647bc46df42a9f2572f0038bafa7a35f859ffb
Rubén Y <rubenmeju@outlook.es>
2023-10-17 14:27:11 +0200

    styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,41 +15,48 @@ export default function Header() {
         />
       </div>
 
-      <section className="max-w-md flex flex-col gap-2 text-xl text-aling">
+      <section className="max-w-md flex flex-col gap-4 text-xl text-aling">
         <div>
           <h1 className="">
-            Hola, mi nombre es
+            Hola, mí nombre es
             <span className="text-sky-500 text-3xl"> Rubén Yáñez.</span>
           </h1>
           <h2 className="">
-            Soy <span className="text-sky-500"> desarrollador web </span>{" "}
+            <span className="text-sky-500">Desarrollador web </span>
             <span className="text-fuchsia-500">Full Stack</span> con sede en La
             Coruña, España.
           </h2>
         </div>
 
         <p className=" text-slate-300 text-lg">
-          Soy un desarrollador autodidacta apasionado por aprender cosas nuevas
-          y <span className="text-fuchsia-500">resolver problemas.</span>{" "}
+          Autodidacta apasionado por aprender cosas nuevas y{" "}
+          <span className="text-fuchsia-500">resolver problemas.</span>
           Actualmente trabajo como freelance, pero estoy buscando nuevas
           oportunidades para crecer como desarrollador.
         </p>
 
-        <div className="flex gap-4">
-          <Image
-            src="/github.svg"
-            alt="github"
-            width={50}
-            height={50}
-            className="cursor-pointer"
-          />
-          <Image
-            src="/linkedin.svg"
-            alt="github"
-            width={50}
-            height={50}
-            className="cursor-pointer"
-          />
+        <div className="flex gap-6">
+          <button className="flex items-center gap-2 border border-fuchsia-500 p-2 rounded-xl hover:border-none hover:bg-sky-500 transition-all duration-500">
+            <Image
+              src="/github.svg"
+              alt="github"
+              width={50}
+              height={50}
+              className="cursor-pointer"
+            />
+            Github
+          </button>
+
+          <button className="flex items-center gap-2 border border-fuchsia-500 p-2 rounded-xl hover:border-none hover:bg-sky-500 transition-all duration-500">
+            <Image
+              src="/linkedin.svg"
+              alt="github"
+              width={50}
+              height={50}
+              className="cursor-pointer"
+            />
+            LinkedIn
+          </button>
         </div>
       </section>
     </div>
